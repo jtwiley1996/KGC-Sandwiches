7895de7d166bd872d411e6def7a042662e60fc04
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-03-18 15:30:20 -0500

    debugging and fixing models

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+const apiRoutes = require('./apis');
 const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
