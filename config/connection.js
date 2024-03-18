c411f607208b47e205b0a2a9dc9c2a5ecccb0c29
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-03-18 00:06:33 -0500

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
-//import the sequelize constructor from the library
-const Sequelize = require('sequelize');
+// Import the sequelize constructor from the library
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
 
-require('dotenv').config();
-//create connection to our database, pass in your MySQL information for username and password
+// Load environment variables from .env file
+dotenv.config();
 
-if(process.env.JAWSDB_URL) {
+// Create connection to our database, pass in your MySQL information for username and password
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host:'localhost',
+        host: 'localhost',
         dialect: 'mysql',
         port: 3306
     });
 }
-module.exports = sequelize;
+
+// Export the Sequelize instance
+export default sequelize;
