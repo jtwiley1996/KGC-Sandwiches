3a1668be4dd35a6891f1a46a888428f3b7d56c1d
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-03-21 16:19:09 -0500

    more css and js

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Add event listener here
+    document.getElementById("signIn").addEventListener("click", function() {
+        // Redirect to homepage
+        window.location.href = "/";
+    });
+});
