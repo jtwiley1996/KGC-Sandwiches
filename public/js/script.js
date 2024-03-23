4fabaa7749e40eb4fef05ad7d68a7df5f34484f6
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-03-22 22:16:08 -0500

    login/register.js

--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Add event listener here
-    document.getElementById("signIn").addEventListener("click", function() {
-        // Redirect to homepage
-        window.location.href = "/";
-    });
-});
